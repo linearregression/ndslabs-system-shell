1ec37f72b48311095bb60de70d3c9add339bdd27
David Raila <raila@illinois.edu>
2016-03-29 15:23:19 -0500

    remove ~/.ndsctl.yaml brefore recreating

--- FILES.system-shell/usr/local/bin/ndslabs-up.sh
+++ FILES.system-shell/usr/local/bin/ndslabs-up.sh
@@ -5,4 +5,5 @@ cat /usr/local/lib/ndslabs/apiserver.yaml | mustache | kubectl create -f-
 
 echo "Creating ~/.ndslabsctl.yaml"
 APISERVER=`kubectl describe svc ndslabs-apiserver | grep ^IP | awk '{print $2}'`
+rm -f ~/.ndslabsctl.yaml
 echo "server: http://$APISERVER:8083" > ~/.ndslabsctl.yaml
