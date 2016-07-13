ba3b30d693b19a2a7f800570d10609970a6bec52
Mike Lambert <lambert8@illinois.edu>
2016-07-13 16:05:28 +0000

    Updated to use new YAML

--- FILES.system-shell/usr/local/bin/ndslabs-up.sh
+++ FILES.system-shell/usr/local/bin/ndslabs-up.sh
@@ -3,3 +3,4 @@
 cat /usr/local/lib/ndslabs/gui.yaml | mustache | kubectl create -f-
 cat /usr/local/lib/ndslabs/apiserver.yaml | mustache | kubectl create -f-
 
+kubectl label nodes 127.0.0.1 ndslabs-node-role="compute"
